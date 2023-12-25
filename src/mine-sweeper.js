64d5ac3ce57ce3a8e72249d4c73d39c00e7dc207
GreyDraco <ancientgreydragon@gmail.com>
2023-12-25 08:40:13 +0300

    feat: solve minesweeper task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const result = [];
+  for (let i = 0; i < matrix.length; i += 1) {
+    const row = [];
+    for (let j = 0; j < matrix[i].length; j += 1) {
+      let count = 0;
+      for (let x = i - 1; x <= i + 1; x += 1) {
+        for (let y = j - 1; y <= j + 1; y += 1) {
+          if (
+            x >= 0 &&
+            x < matrix.length &&
+            y >= 0 &&
+            y < matrix[i].length &&
+            !(x === i && y === j)
+          ) {
+            count += matrix[x][y] ? 1 : 0;
+          }
+        }
+      }
+      row.push(count);
+    }
+    result.push(row);
+  }
+  return result;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
